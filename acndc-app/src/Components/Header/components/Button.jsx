070c521830bf4053dd222d63d317c0bd2b9c5efe
unknown <obediobadiah05@gmail.com>
2021-10-05 15:55:16 +0200

    Acndc Header Commit

---
+++ acndc-app/src/Components/Header/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import  '../styles/Button.css'
+
+function Button() {
+	return (
+		<div className="Buttono-container">
+			<button className="btn">DONATE</button>
+		</div>
+	)
+}
+
+export default Button
+
---
+++ acndc-app/src/Components/Header/header.jsx
@@ -0,0 +1,109 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import ActivitesDropdown from './components/ActivitesDropdown'
+import SommeNousDropdown from './components/SommeNousDropdown'
+import  './style.css'
+import logoImg from './assets/Logos ACNDC.png'
+
+
+
+function Header() {
+
+	const [click, setClick] = useState(false);
+	const [Actdropdown, setActDropdown] = useState(false);
+	const [SomDropdown, setSomDropdown] = useState(false);
+	const handleClick = () => setClick(!click);
+	const closeMobileMenu = () => setClick(false)
+
+
+
+	const onMouseEnterAct = () => {
+    if (window.innerWidth < 960) {
+      setActDropdown(true);
+	  
+    } else {
+      setActDropdown(true);
+     
+    }
+  };
+
+  const onMouseLeaveAct = () => {
+    if (window.innerWidth < 960) {
+      setActDropdown(false);
+      
+    } else {
+      setActDropdown(false);
+      
+    }
+  };
+
+	const onMouseEnterSom = () => {
+    if (window.innerWidth < 960) {
+      
+	  setSomDropdown(true);
+    } else {
+    
+      setSomDropdown(true);
+    }
+  };
+
+  const onMouseLeaveSom = () => {
+    if (window.innerWidth < 960) {
+      
+      setSomDropdown(false);
+    } else {
+      
+      setSomDropdown(false);
+    }
+  };
+
+	return (
+		<>
+			<div className="header">
+				<div className="menu-icon" onClick={handleClick}>
+					<i className={click ? 'fas fa-times': 'fas fa-bars'} />
+				</div>
+				<div className="header-details">
+					<div className="logo">
+						<Link to="/" className="header-logo">
+						<img src={logoImg} alt="Site logo" className="logoimg" />
+						</Link>
+					</div>
+					<div className="header-menu-content">
+						<ul className={click? 'header-menu active': 'header-menu'}>
+							<li className="header-item"
+								onMouseEnter={onMouseEnterAct}
+								onMouseLeave={onMouseLeaveAct}>
+								<Link to="/" className="header-links" onClick={closeMobileMenu}>Nos Activités <i className="fas fa-caret-down"/></Link>
+								{Actdropdown && <ActivitesDropdown />}
+							</li>
+
+							<li className="header-item"
+								onMouseEnter={onMouseEnterSom}
+								onMouseLeave={onMouseLeaveSom}>
+								<Link to="/" className="header-links" onClick={closeMobileMenu}>Qui somme-nous? <i className="fas fa-caret-down"/></Link>
+								{SomDropdown && <SommeNousDropdown />}
+							</li>
+
+
+							<li className="header-item">
+								<Link to="/" className="header-links" onClick={closeMobileMenu}>Nos Actualités </Link>
+							</li>
+
+
+							<li className="header-item">
+								<Link to="/" className="header-links" onClick={closeMobileMenu}>Contact </Link>
+							</li>
+							
+						</ul>
+					</div>
+					<div className="Button-container">
+						<button className="btn">DONATE</button>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default Header
