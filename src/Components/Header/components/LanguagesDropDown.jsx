4ff11ab75690d0897c43161ccc738fc9338d02d4
Obedi Obadiah <obediobadiah@Obedis-MacBook-Pro.local>
2023-03-03 23:18:46 +0200

    i18Next Language Translation

--- src/Components/Header/components/LanguagesDropDown.jsx
+++ src/Components/Header/components/LanguagesDropDown.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import '../styles/DropdownAct.css';
+import '../styles/DropdownLang.css';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next'
 
