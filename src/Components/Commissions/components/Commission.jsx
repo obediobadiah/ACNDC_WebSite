ea2ba4127ce2d0f80a7de4975eb8ec39187bdee1
Obedi Obadiah <obediobadiah@Obedis-MacBook-Pro.local>
2023-06-01 13:15:39 +0200

    commisions translation

--- src/Components/Commissions/components/Commission.jsx
+++ src/Components/Commissions/components/Commission.jsx
@@ -2,29 +2,33 @@ import React from 'react'
 import Header from '../../Header/header'
 import Footer from '../../Footer/components/Footer'
 import '../styles/style.css'
+import { useTranslation } from 'react-i18next'
 
 function Commission() {
+
+	const { t } = useTranslation();
+	
 	return (
 		<div className="commision_container">
 			<Header />
 			<div className="commissions_contents">
 				<div className="commissionss_detatils">
 					<div className="commissions_Intro">
-						<h1>NOS COMMISSIONS</h1>
+						<h1>{t("commissions_intro_title")}</h1>
 					</div>
 
 						<div className="commissions_textes">
-							<p>Nos commissions ont la charge d’accomplir de faire des évaluations et des études sur les questions liées aux programmes de ACNDC, pour l’accomplissement de notre mission, nous avons quatre commissions.</p>
+							<p>{t("commissions_textes_head")}</p>
 
-							<h1>La commission de la conservation de la nature, de la préservation de la culture et du patrimoine ancestral.</h1>
+							<h1>{t("commissions_textes_conservation")}</h1>
 
-							<h1>La commission du développement communautaire, de la sauvegarde de l’environnement et des écosystèmes paysagers.</h1>
+							<h1>{t("commissions_textes_development")}</h1>
 
-							<h1>La commission de la masculinité positive et de l’activité féminines dans les secteurs socioéconomiques à dominance des hommes patriarches, époux et garçons.</h1>
+							<h1>{t("commissions_textes_masculinite")}</h1>
 
-							<h1>La commission des générations futures, de la préparation professionnelle communautaire et de la croissance vocationnelle économique territoriale.</h1>
+							<h1>{t("commissions_textes_generation")}</h1>
 							
-							<h1>La commission de la coopération internationale, du commerce extérieur et de la promotion linguistique mondiale.</h1>
+							<h1>{t("commissions_textes_cooperation")}</h1>
 						</div>
 				</div>
 			</div>
