c7d34f93765c6d1b593f2ea0e4b61b82f0d5263c
Obedi Obadiah <obediobadiah@Obedis-MacBook-Pro.local>
2023-06-02 08:56:41 +0200

    approches translation

--- src/Components/Approches/components/Approches.jsx
+++ src/Components/Approches/components/Approches.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 import Header from '../../Header/header'
 import Footer from '../../Footer/components/Footer'
 import '../styles/style.css'
+import { useTranslation } from 'react-i18next'
 
 function Approches() {
+
+	const { t } = useTranslation();
+
 	return (
 		<div className="Approches_container">
 			<Header />
 			<div className="Approches_contents">
 				<div className="Approches_detatils">
 					<div className="Approches_Intro">
-						<h1>NOS APPROCHES</h1>
+						<h1>{t("approche_intro_title")}</h1>
 					</div>
 
 
@@ -23,11 +27,10 @@ function Approches() {
 								<h1>1</h1>
 							</div>
 							<div className="Approche">
-								<p>APPROCHE CYCLIQUE ET PERIODIQUE</p>
+								<p>{t("approche_cyclique_h")}</p>
 							</div>
 							<div className="Approche_exp">
-								<p>ACNDC travaille par activités (opérations/interventions). Celles-ci sont autant continues que discontinues, intégrées ou désintégrées les unes des autres. Suivant un processus cyclique périodique (échéancier), les activités commencent et se terminent. Il est ainsi mis en place un service de coordination et d’appui aux projets (SCAP).
-								</p>
+								<p>{t("approche_cyclique_p")}</p>
 							</div>
 						</div>
 
@@ -38,13 +41,10 @@ function Approches() {
 								<h1>2</h1>
 							</div>
 							<div className="Approche">
-								<p>APPROCHE INSTITUTIONNELLE, ORGANISATIONNELLE ET STRUCTURELLE.</p>
+								<p>{t("approche_instit_h")}</p>
 							</div>
 							<div className="Approche_exp">
-								<p>
-									Cette approche repose sur la cohérence de la structuration fonctionnelle (ressources), stratégiques et idéologiques à tous les niveaux centraux, intermédiaires et décentralisés. Les conseils d’administration et les commissions de conservation de la nature, de développement durable et celle de décentralisation disposent
-									de leurs orientations, leurs gestions et de leurs résultats. A cet effet, chaque structure est une institution à part entière et est en relations hiérarchiques et de collaboration avec d’autres. ACNDC fonction ainsi en réseau et mouvement autonome.
-								</p>
+								<p>{t("approche_instit_p")}</p>
 							</div>
 						</div>
 
@@ -55,12 +55,10 @@ function Approches() {
 								<h1>3</h1>
 							</div>
 							<div className="Approche">
-								<p>APPROCHE CONTEXTUELLE ET EVOLUTIVE (DYNAMIQUE).</p>
+								<p>{t("approche_const_h")}</p>
 							</div>
 							<div className="Approche_exp">
-								<p>
-								ACNDC surveille continuellement le contexte de sa zone d’action afin de se positionner et d’introduire des adaptations de mise en œuvre (innovation, extension, réplication, accélération, diversification) de ses activités.
-								</p>
+								<p>{t("approche_const_p")}</p>
 							</div>
 						</div>
 					</div>
