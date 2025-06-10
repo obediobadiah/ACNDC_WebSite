a0906f9c21536009f6bd898d5867fab0020aa962
obediobadiah <obediobadiah05@gmail.com>
2025-06-10 13:41:12 +0200

    [ 2024 Annaul Rapport ]

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import Rapports_Details_2020 from './Components/Rapports/components/RapportsAnnu
 import Rapports_Details_2021 from './Components/Rapports/components/RapportsAnnuel/Rapports_detail_2021'
 import Rapports_Details_2022 from './Components/Rapports/components/RapportsAnnuel/Rapports_detail_2022'
 import Rapports_Details_2023 from './Components/Rapports/components/RapportsAnnuel/Rapports_detail_2023'
+import Rapports_Details_2024 from './Components/Rapports/components/RapportsAnnuel/Rapports_detail_2024'
 import OutilsTravail from './Components/OutilsTravail/components/OutilsTravail'
 import Plans from './Components/Plans/components/Plans'
 import Actualite from './Components/Actualite/components/Actualite'
@@ -47,6 +48,7 @@ function App() {
         <Route path="/Rapport_Detail_2021" element={<Rapports_Details_2021 />} />
         <Route path="/Rapport_Detail_2022" element={<Rapports_Details_2022 />} />
         <Route path="/Rapport_Detail_2023" element={<Rapports_Details_2023 />} />
+        <Route path="/Rapport_Detail_2024" element={<Rapports_Details_2024 />} />
         <Route path="/OutilsTravail" element={<OutilsTravail />} />
         <Route path="/Plans" element={<Plans />} />
         <Route path="/Actualite" element={<Actualite />} />
